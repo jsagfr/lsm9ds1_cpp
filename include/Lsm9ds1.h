fa70ef1df8e5a4fc94a03c37b699e357675bd1bb
Jérôme Guéry <jerome.guery@gmail.com>
2016-12-22 20:44:06 +0000

    First ideas

---
+++ include/Lsm9ds1.h
@@ -0,0 +1,114 @@
+#ifndef LSM9DS1_H
+#define LSM9DS1_H
+
+#include "I2cDevice.h"
+
+#include <string>
+#include <cstdint>
+#include <vector>
+/* enum Direction: int {A = 1, B = -1}; */
+
+enum RegType {
+  CTRL_REG6_XL
+};
+
+enum ParamType {
+  TypeFsXl
+};
+
+enum class FsXl : int {
+  Fs2g = 2,
+  Fs4g = 4,
+  Fs8g = 8,
+  Fs16g = 16
+};
+enum class FsXlMask : uint8_t {
+  Fs2g = 0b00000000,
+  Fs4g = 0b00010000,
+  Fs8g = 0b00011000,
+  Fs16g = 0b00001000,
+  Mask = 0b00011000
+};
+enum class OdrG: uint8_t {
+  A = 0
+};
+enum class OdrXl: uint8_t {
+  A = 0
+};
+// enum OutputDataRate: uint8_t {
+//   C = 0
+// };
+// enum Md: uint8_t {
+//   D = 0
+// };
+
+
+class ParamFsXl
+{
+public:
+  ParamFsXl(FsXl value);
+  ParamFsXl();
+  ~ParamFsXl();
+
+  FsXl operator()();
+  uint8_t regMask();
+  uint8_t regValue();
+  RegType regType();
+  ParamType paramType();
+  
+private:
+  FsXl _value;
+  uint8_t _regMask;
+  RegType _regType;
+  ParamType _paramType;
+};
+
+
+
+class Lsm9ds1Config
+{
+public:
+  Lsm9ds1Config();
+  ~Lsm9ds1Config();
+
+  ParamFsXl fsXl;
+  // ParamOdrG odrG;
+  // ParamOdrXl odrXl;
+  // ParamOutputDataRate outputDataRate;
+  // ParamMd md;
+
+};
+
+class Lsm9ds1
+{
+protected:
+  I2cDevice _i2cDevice;
+  Lsm9ds1Config _config;
+
+public:
+  Lsm9ds1(const std::string& i2cPath, Lsm9ds1Config config);
+  Lsm9ds1(const std::string& i2cPath);
+  ~Lsm9ds1();
+
+  std::vector<float> l();
+  float lx();
+  float ly();
+  float lz();
+
+  float gx();
+  float gy();
+  float gz();
+
+  float mx();
+  float my();
+  float mz();
+
+  void config(Lsm9ds1Config config);
+
+private:
+  uint16_t read(uint8_t reg);
+  float registerTo(int16_t regValue);
+
+};
+
+#endif	// LSM9DS1
---
+++ src/Lsm9ds1.cpp
@@ -0,0 +1,140 @@
+#include "Lsm9ds1.h"
+#include <limits>
+
+const uint8_t lsm9ds1I2cAddr = 0x00;
+const uint8_t OUT_X_XL = 0x28;
+
+
+ParamFsXl::ParamFsXl(FsXl value) :
+  _value(value),
+  _regMask(0b00011000),
+  _regType(RegType::CTRL_REG6_XL),
+  _paramType(ParamType::TypeFsXl)
+{}
+
+ParamFsXl::ParamFsXl() :
+  ParamFsXl(FsXl::Fs8g)
+{}
+
+ParamFsXl::~ParamFsXl()
+{}
+
+FsXl ParamFsXl::operator()()
+{
+  return _value;
+}
+uint8_t ParamFsXl::regMask()
+{
+  return _regMask;
+}
+uint8_t ParamFsXl::regValue()
+{
+  return static_cast<int>(_value);
+}
+RegType ParamFsXl::regType()
+{
+  return _regType;
+}
+ParamType ParamFsXl::paramType()
+{
+  return _paramType;
+}
+
+
+
+Lsm9ds1::Lsm9ds1(const std::string& i2cPath, Lsm9ds1Config config) :
+  _i2cDevice(i2cPath, lsm9ds1I2cAddr),
+  _config(config)
+{}
+
+Lsm9ds1::Lsm9ds1(const std::string& i2cPath) :
+  Lsm9ds1(i2cPath, Lsm9ds1Config{})
+{}
+
+Lsm9ds1::~Lsm9ds1()
+{
+}
+
+uint16_t Lsm9ds1::read(uint8_t reg)
+{
+  _i2cDevice.write({reg});
+  uint16_t data = *_i2cDevice.read<uint16_t>();
+  return data;
+}
+
+float Lsm9ds1::registerTo(int16_t regValue)
+{
+  return _config.fs_xl.value() *
+    statc_cast<float>(regValue) / std::numeric_limits<float>max();
+}
+
+std::vector<float> Lsm9ds1::l()
+{
+  std::vector<float> v{0};
+  return v;
+}
+
+float Lsm9ds1::lx()
+{
+  int16_t v = read(OUT_X_XL);
+  // config.fsXl
+  return static_cast<float>(v);
+}
+
+float Lsm9ds1::ly()
+{
+  return 0.0;
+}
+
+float Lsm9ds1::lz()
+{
+  return 0.0;
+}
+
+
+float Lsm9ds1::gx()
+{
+  return 0.0;
+}
+
+float Lsm9ds1::gy()
+{
+  return 0.0;
+}
+
+float Lsm9ds1::gz()
+{
+  return 0.0;
+}
+
+
+float Lsm9ds1::mx()
+{
+  return 0.0;
+}
+
+float Lsm9ds1::my()
+{
+  return 0.0;
+}
+
+float Lsm9ds1::mz()
+{
+  return 0.0;
+}
+
+
+void Lsm9ds1::config(Lsm9ds1Config config)
+{
+  _config = config;
+}
+
+
+
+
+Lsm9ds1Config::Lsm9ds1Config() :
+  fsXl(ParamFsXl{})
+{}
+
+Lsm9ds1Config::~Lsm9ds1Config()
+{}
