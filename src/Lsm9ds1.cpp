e4c9ae91c10264403b71c3eabaa53014680549a8
Jérôme Guéry <jerome.guery@gmail.com>
2016-12-29 16:00:29 +0000

    clean Param part in Lsm9ds1

--- src/Lsm9ds1.cpp
+++ src/Lsm9ds1.cpp
@@ -3,114 +3,12 @@
 #include "Param.h"
 #include <limits>
 
+
 const uint8_t lsm9ds1I2cAddr = 0x6b;
 const uint8_t OUT_X_XL = 0x28;
 const uint8_t OUT_Y_XL = 0x2A;
 const uint8_t OUT_Z_XL = 0x2C;
 const uint8_t WHO_AM_I = 0x0f;
-
-Parameter::Parameter(uint8_t regValue,
-		     uint8_t regMask,
-		     RegType regType,
-		     ParamType paramType) :
-  _regValue(regValue),
-  _regMask(regMask),
-  _regType(regType),
-  _paramType(paramType)
-{}
-
-uint8_t Parameter::regMask()
-{
-  return _regMask;
-}
-
-uint8_t Parameter::regValue()
-{
-  return _regValue;
-}
-
-RegType Parameter::regType()
-{
-  return _regType;
-}
-
-ParamType Parameter::paramType()
-{
-  return _paramType;
-}
-
-ParamFsXl::ParamFsXl(FsXl value) :
-  Parameter(0,
-	    static_cast<uint8_t>(FsXlMask::Mask),
-	    RegType::CTRL_REG6_XL,
-	    ParamType::FsXl),
-  _value(value)
-{
-  switch (_value) {
-  case FsXl::Fs2g:
-    _regValue = static_cast<uint8_t>(FsXlMask::Fs2g);
-    break;
-  case FsXl::Fs4g:
-    _regValue = static_cast<uint8_t>(FsXlMask::Fs4g);
-    break;
-  case FsXl::Fs8g:
-    _regValue = static_cast<uint8_t>(FsXlMask::Fs8g);
-    break;
-  case FsXl::Fs16g:
-    _regValue = static_cast<uint8_t>(FsXlMask::Fs16g);
-    break;
-  }
-}
-
-ParamFsXl::ParamFsXl() :
-  ParamFsXl(FsXl::Fs2g)
-{}
-
-int ParamFsXl::operator()()
-{
-  return static_cast<int>(_value);
-}
-
-ParamOdrXl::ParamOdrXl(OdrXl value) :
-  Parameter(0,
-	    static_cast<uint8_t>(OdrXlMask::Mask),
-	    RegType::CTRL_REG6_XL,
-	    ParamType::OdrXl),
-  _value(value)
-{
-  switch (_value) {
-  case OdrXl::PowerDown:
-    _regValue = static_cast<uint8_t>(OdrXlMask::PowerDown);
-    break;
-  case OdrXl::F10Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F10Hz);
-    break;
-  case OdrXl::F50Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F50Hz);
-    break;
-  case OdrXl::F119Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F119Hz);
-    break;
-  case OdrXl::F238Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F238Hz);
-    break;
-  case OdrXl::F476Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F476Hz);
-    break;
-  case OdrXl::F952Hz:
-    _regValue = static_cast<uint8_t>(OdrXlMask::F952Hz);
-    break;
-  }
-}
-
-ParamOdrXl::ParamOdrXl() :
-  ParamOdrXl(OdrXl::PowerDown)
-{}
-
-int ParamOdrXl::operator()()
-{
-  return static_cast<int>(_value);
-}
 
 
 Lsm9ds1::Lsm9ds1(const std::string& i2cPath, Lsm9ds1Config config) :
@@ -213,22 +111,3 @@ void Lsm9ds1::config(Lsm9ds1Config config)
   _config = config;
 }
 
-
-
-
-Lsm9ds1Config::Lsm9ds1Config() :
-  fsXl(ParamFsXl{})
-{}
-
-Lsm9ds1Config::~Lsm9ds1Config()
-{}
-
-std::unordered_map<uint8_t, uint8_t> Lsm9ds1Config::registers()
-{
-  std::unordered_map<uint8_t, uint8_t> registers;
-
-  registers[static_cast<uint8_t>(fsXl.regType())] |= fsXl.regValue();
-  registers[static_cast<uint8_t>(odrXl.regType())] |= odrXl.regValue();
-
-  return registers;
-}
